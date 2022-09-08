068ab68c8eb4576f6a5233c1a0a8d59912f8a45e
timlamec <timlamec@gmail.com>
2022-09-08 09:26:02 +0300

    created simple webmap that displays geojson data

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const map = L.map("map").setView([0.347690, 35.703807], 9);
+const map = L.map("map").setView([-6.785568, 39.261278], 13);
 const osm = L.tileLayer(
   "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
   {
@@ -8,47 +8,19 @@ const osm = L.tileLayer(
   }
 ).addTo(map);
 
-const geojsonMarkerOptions = {
-  radius: 8,
-  fillColor: "#ff7800",
-  color: "#000",
-  weight: 1,
-  opacity: 1,
-  fillOpacity: 0.8,
-};
 
- // loading geojson
-const dataset = L.geoJSON(table, {
-  onEachFeature: function (feature, layer) {
+fetch("data/Mikocheni.geojson")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      L.geoJSON(data).addTo(map);
+    })
 
-         const popupContent =
-        '<h4 class = "text-primary">Sample Point</h4>' +
-        '<div class="container"><table class="table table-striped">' +
-        "<thead><tr><th>Properties</th><th>Value</th></tr></thead>" +
-        "<tbody><tr><td> CouName </td><td>" +
-        feature.properties.CouName +
-        "</td></tr>" +
-        "<tr><td>SCouName </td><td>" +
-        feature.properties.SCouName +
-        "</td></tr>" +
-        "<tr><td> DivName</td><td>" +
-        feature.properties.DivName +
-        "</td></tr>" +
-        "<tr><td> LocName </td><td>" +
-        feature.properties.LocName +
-        "</td></tr>" +
-        "<tr><td> SLName </td><td>" +
-        feature.properties.SLName +
-        "</td></tr>";
-        
-        layer.bindPopup(popupContent);
-
-  },
-  pointToLayer: function (feature, latlng) {
-    return L.circleMarker(latlng, geojsonMarkerOptions);
-  },
-});
-
-const markers = L.markerClusterGroup().addLayer(dataset);
-// marker clustering
-map.addLayer(markers);
+fetch("data/Mikocheni_dumb_grid.geojson")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      L.geoJSON(data).addTo(map);
+    })
